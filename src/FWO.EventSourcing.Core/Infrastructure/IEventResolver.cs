abbcd3f871af001bf81d6e1e305b5b1d28cf01f2
Jordan Lee <jordan@jordanlee.net>
2022-09-28 16:43:35 +1000

    Add EventResolver interface

---
+++ src/FWO.EventSourcing.Core/Infrastructure/IEventResolver.cs
@@ -0,0 +1,14 @@
+﻿using FWO.EventSourcing.Core.Events;
+
+namespace FWO.EventSourcing.Core.Infrastructure
+{
+    public interface IEventResolver
+    {
+        /// <summary>
+        /// Resolves a list of events derived from <see cref="BaseEvent"/> from <see cref="EventModel.EventData"/>.
+        /// </summary>
+        /// <param name="events"></param>
+        /// <returns></returns>
+        List<BaseEvent> ResolveEvents(List<EventModel> eventModels);
+    }
+}
