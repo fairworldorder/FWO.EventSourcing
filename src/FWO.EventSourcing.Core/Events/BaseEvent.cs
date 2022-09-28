34d3301d85eeb99418fed9659b5e58f76696aa39
Jordan Lee <jordan@jordanlee.net>
2022-09-28 16:44:20 +1000

    Remove unused properties from BaseEvent

--- src/FWO.EventSourcing.Core/Events/BaseEvent.cs
+++ src/FWO.EventSourcing.Core/Events/BaseEvent.cs
@@ -8,22 +8,22 @@ namespace FWO.EventSourcing.Core.Events
         {
         }
 
-        protected BaseEvent(string type)
-        {
-            Type = type;
-        }
+        //protected BaseEvent(string type)
+        //{
+        //    Type = type;
+        //}
 
         [JsonProperty("id")]
         public string Id { get; set; }
 
-        [JsonProperty("version")]
-        public int Version { get; set; }
+        //[JsonProperty("version")]
+        //public int Version { get; set; }
 
         /// <summary>
         /// Discriminator property; Used for polymorphic data binding,
         /// when deserializing event objects.
         /// </summary>
-        [JsonProperty("type")]
-        public string Type { get; set; }
+        //[JsonProperty("type")]
+        //public string Type { get; set; }
     }
 }
